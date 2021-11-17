dd7c1911609501a45fdfda457eacd0a78fff5887
savio591 <savio591@hotmail.com>
2021-11-17 10:23:18 -0300

    fix: dateFormat - Intl to date-fns | resolves #6

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -1,7 +1,22 @@
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
 export function dateFormat(dateISO: string | undefined | null): string {
-  return new Intl.DateTimeFormat("pt-BR", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  }).format(new Date(dateISO || "0"));
+  const dateFormattedLowerCase = format(
+    parseISO(dateISO || "0"),
+    "MMM dd, yyyy",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const firstCharacter = dateFormattedLowerCase.substring(0, 1);
+  const restStrings = dateFormattedLowerCase.substring(1);
+
+  return firstCharacter.toUpperCase() + restStrings;
+  // return new Intl.DateTimeFormat("pt-BR", {
+  //   month: "short",
+  //   day: "numeric",
+  //   year: "numeric",
+  // }).format(new Date(dateISO || "0"));
 }
