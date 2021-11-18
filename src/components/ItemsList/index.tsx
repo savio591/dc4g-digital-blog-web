883c50a613f4c154acfec11051056396cf9446f0
savio591 <savio591@hotmail.com>
2021-11-18 00:52:35 -0300

    styles: component ItemsList

--- src/components/ItemsList/index.tsx
+++ src/components/ItemsList/index.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import { useMemo } from "react";
 import { dateFormat } from "../../utils/dateFormat";
 
+import { MdEditCalendar } from "react-icons/md";
+
+import styles from "./itemsList.module.scss";
+
 export type ItemProps = {
   imageAlt: string;
   imageUrl: string;
@@ -20,16 +24,19 @@ interface ItemsListProps {
 export function ItemsList({ posts }: ItemsListProps) {
   if (typeof posts !== "object" || posts.length <= 0) {
     return (
-      <div>
-        <p>
+      <div className={styles.container} role="contentinfo">
+        <p role="alert">
           <span>{"Nenhum item para mostrar :-("}</span>
         </p>
       </div>
     );
   }
-  return <ul>{posts.map((post) => ItemBase({ ...post }))}</ul>;
+  return (
+    <ul className={styles.container}>
+      {posts.map((post) => ItemBase({ ...post }))}
+    </ul>
+  );
 }
-
 export const ItemBase = ({
   imageUrl,
   imageAlt,
@@ -44,21 +51,22 @@ export const ItemBase = ({
   const updatedAtFormatted = useMemo(() => dateFormat(updatedAt), [updatedAt]);
 
   return (
-    <li key={slug}>
-      <div>
+    <li key={slug} className={styles.card}>
+      <div className={styles.imgDiv}>
         <Image src={imageUrl} layout="fill" alt={imageAlt} />
       </div>
       <section>
-        <span>{category}</span>
+        <p className={styles.category}>{category}</p>
         <article>
           <h3>{title}</h3>
           <p>{description}</p>
         </article>
 
         <p role="contentinfo">
-          Publicado:
+          <MdEditCalendar size={24} className={styles.icon} />
+          {"Publicado: "}
           <time dateTime={createdAt}>{createdAtFormatted}</time>
-          {updatedAt && "- Atualizado:"}
+          {updatedAt && " - Atualizado: "}
           {updatedAt && <time dateTime={updatedAt}>{updatedAtFormatted}</time>}
         </p>
       </section>
