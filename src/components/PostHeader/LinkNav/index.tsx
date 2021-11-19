3d6d210369c104b55a8860b09ba376a4e8f96c61
savio591 <savio591@hotmail.com>
2021-11-19 19:39:25 -0300

    chore: same icons of the prototype

--- src/components/PostHeader/LinkNav/index.tsx
+++ src/components/PostHeader/LinkNav/index.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
-import { FaExternalLinkAlt, FaFacebook, FaTwitter } from "react-icons/fa";
+import { FaFacebookSquare, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { MdLink } from "react-icons/md";
+
+import styles from "./LinkNav.module.scss";
 
 interface LinkNavProps {
   uri: string;
@@ -15,35 +18,46 @@ export function LinkNav({ uri, text }: LinkNavProps) {
   const twitterLink = `https://twitter.com/share?url=${uriEncoded}&text=${textEncoded}`;
   const whatsappLink = `https://api.whatsapp.com/send?text=${textWithLinkEncoded}`;
 
+  function handleShareButton() {
+    navigator.clipboard.writeText(uri);
+  }
+
   return (
-    <nav>
-      <span>Compartilhe:</span>
+    <nav className={styles.container}>
+      <span>Compartilhe: </span>
       <ul>
         <li>
           <Link href={facebookLink}>
-            <a title="Compartilhar post no Facebook">
-              <FaFacebook />
+            <a title="Compartilhar post no Facebook" target='_blank'>
+              <FaFacebookSquare size={24} />
             </a>
           </Link>
         </li>
         <li>
           <Link href={twitterLink}>
-            <a title="Compartilhar post no Twitter">
-              <FaTwitter />
+            <a title="Compartilhar post no Twitter" target='_blank'>
+              <FaTwitter size={24} />
             </a>
           </Link>
         </li>
         <li>
           <Link href={whatsappLink}>
-            <a title="Compartilhar post no WhatsApp">
-              <FaTwitter />
+            <a title="Compartilhar post no WhatsApp" target='_blank'>
+              <FaWhatsapp size={24} />
             </a>
           </Link>
         </li>
         <li>
           <Link href={uri}>
-            <a title="Copiar link">
-              <FaExternalLinkAlt />
+            <a
+              title="Copiar link"
+              target='_blank'
+              onClick={(event) => {
+                event.preventDefault();
+                handleShareButton();
+              }}
+            >
+              <MdLink size={24} />
             </a>
           </Link>
         </li>
